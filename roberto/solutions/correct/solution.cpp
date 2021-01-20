eb1c2415fe2595d596a19e08378108ea9c29eb6b
Nico Lehmann <nico.lehmannm@gmail.com>
2021-01-19 22:12:12 -0800

    nit

--- roberto/solutions/correct/solution.cpp
+++ roberto/solutions/correct/solution.cpp
@@ -29,38 +29,38 @@ pair<vector<int>, vector<int>> compute_min_max_arrs(const vector<int> &arr,
 }
 
 int main() {
-  int M, N, A, B;
-  scanf("%d%d%d%d", &M, &N, &A, &B);
+  int H, W, M, N;
+  scanf("%d%d%d%d", &H, &W, &M, &N);
 
-  vector<vector<int>> terrain(M, vector<int>(N));
+  vector<vector<int>> terrain(H, vector<int>(W));
 
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
+  for (int i = 0; i < H; ++i) {
+    for (int j = 0; j < W; ++j) {
       scanf("%d", &terrain[i][j]);
     }
   }
 
-  vector<vector<int>> mins_for_row(M);
-  vector<vector<int>> maxs_for_row(M);
-  for (int i = 0; i < M; ++i) {
-    tie(mins_for_row[i], maxs_for_row[i]) = compute_min_max_arrs(terrain[i], B);
+  vector<vector<int>> mins_for_row(H);
+  vector<vector<int>> maxs_for_row(H);
+  for (int i = 0; i < H; ++i) {
+    tie(mins_for_row[i], maxs_for_row[i]) = compute_min_max_arrs(terrain[i], N);
   }
 
   int best = INT_MAX;
-  for (int j = 0; j + B - 1 < N; ++j) {
+  for (int j = 0; j + N - 1 < W; ++j) {
     multiset<int> elements_min;
     multiset<int> elements_max;
 
-    for (int i = 0; i < A; ++i) {
+    for (int i = 0; i < M; ++i) {
       elements_min.insert(mins_for_row[i][j]);
       elements_max.insert(maxs_for_row[i][j]);
     }
     best = min(best, *elements_max.rbegin() - *elements_min.begin());
-    for (int i = 1; i + A - 1 < M; ++i) {
+    for (int i = 1; i + M - 1 < H; ++i) {
       elements_min.erase(mins_for_row[i - 1][j]);
-      elements_min.insert(mins_for_row[i + A - 1][j]);
+      elements_min.insert(mins_for_row[i + M - 1][j]);
       elements_max.erase(maxs_for_row[i - 1][j]);
-      elements_max.insert(maxs_for_row[i + A - 1][j]);
+      elements_max.insert(maxs_for_row[i + M - 1][j]);
 
       best = min(best, *elements_max.rbegin() - *elements_min.begin());
     }
