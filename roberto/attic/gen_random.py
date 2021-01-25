dcf77cf70d5e15e6b964fb49103d09d927fc77e7
Diego <diegosalas387@gmail.com>
2021-01-25 06:25:00 -0300

    Testplan for roberto

--- roberto/attic/gen_random.py
+++ roberto/attic/gen_random.py
@@ -8,11 +8,25 @@ w = int(sys.argv[3])
 m = int(sys.argv[4])
 n = int(sys.argv[5])
 
+modo = int(sys.argv[6])
+aux1 = int(sys.argv[7])
+aux2 = int(sys.argv[8])
+
+maxlim = 1000
+
 print(h, w, m, n)
 
 for i in range(h):
-    for j in range(w):
-        if j == w - 1:
-            print(random.randint(1, 1000))
-        else:
-            print(random.randint(1, 1000), end = " ")
+	for j in range(w):
+
+		if modo == 1:
+			if j == w - 1:
+				print(random.randint(1, maxlim))
+			else:
+				print(random.randint(1, maxlim), end = " ")
+
+		if modo == 2:
+			if j == w - 1:
+				print(random.randint(max(0, aux1 - aux2), min(aux1 + aux2, maxlim)))
+			else:
+				print(random.randint(max(0, aux1 - aux2), min(aux1 + aux2, maxlim)), end = " ")
