d0495a0a1b2f341bad98d3ce0ca832ba59bd7c8e
Nico Lehmann <nico.lehmannm@gmail.com>
2021-01-27 14:40:44 -0800

    Fix dataset catalonia

--- catalonia/attic/GenFixed0.py
+++ catalonia/attic/GenFixed0.py
@@ -4,28 +4,4 @@ import sys
 random.seed(str(sys.argv[1:]))
 N = int(sys.argv[2])
 
-print("0 " + str(random.randint(50, N))
-
-
-# import random
-# 
-
-# random.seed(str(sys.argv[1:]))
-
-# N = int(sys.argv[2])
-# P = int(sys.argv[3])
-
-# assert(P <= (N * (N - 1)) / 2)
-
-
-# def pairs_iter(N):
-#     for a in range(N):
-#         for b in range(a + 1, N):
-#             yield (a, b)
-
-
-# print(N, P)
-
-# for (a, b) in random.sample(list(pairs_iter(N)), P):
-#     w = random.choice([a, b, -1])
-#     print(a, b, w)
+print(0, random.randint(50, N))
--- catalonia/attic/GenRandom.py
+++ catalonia/attic/GenRandom.py
@@ -4,7 +4,7 @@ import sys
 random.seed(str(sys.argv[1:]))
 N = int(sys.argv[2])
 
-a = random.randint(0, N)
-b = random.randint(0, N)
+a = random.randint(0, N - 1)
+b = random.randint(a + 1, N)
 
-print(str(a) + " " + str(b))
+print(a, b)
