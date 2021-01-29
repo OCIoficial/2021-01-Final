1372d56bbad40184ad246696023c6f9fc23407b7
Nico Lehmann <nico.lehmannm@gmail.com>
2021-01-29 14:46:43 -0800

    Finalize ajedrez

--- ajedrez/solutions/correct/ralonso.cpp
+++ ajedrez/solutions/correct/ralonso.cpp
@@ -1,33 +1,34 @@
-#include<cstdio>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    int n, p;
+  int n, p;
 
-    scanf("%d%d", &n, &p);
+  scanf("%d%d", &n, &p);
 
-    vector<int> scores(n, 0);
+  vector<int> scores(n, 0);
 
-    for (int i = 0; i < p; i++) {
-        int a, b, c;
-        scanf("%d%d%d", &a, &b, &c);
-        
-        if (c == -1) {
-            scores[a]++;
-            scores[b]++;
-        } else {
-            scores[c] += 2;
-        }
+  for (int i = 0; i < p; i++) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (c == -1) {
+      scores[a]++;
+      scores[b]++;
+    } else {
+      scores[c] += 2;
     }
-    int max_score = *max_element(begin(scores), end(scores));
+  }
+  int max_score = *max_element(begin(scores), end(scores));
 
-    for (int i = 0; i < n; i++) {
-        if (scores[i] == max_score) {
-            printf("%d\n", i);
-        }
+  // Print in descending order to verify the checker is working properly.
+  for (int i = n - 1; i >= 0; i--) {
+    if (scores[i] == max_score) {
+      printf("%d\n", i);
     }
-    return 0;
+  }
+  return 0;
 }
