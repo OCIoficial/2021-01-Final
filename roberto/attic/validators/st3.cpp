ff1cdf85fcb138718863346e67bca5c7a30c8c00
Nico Lehmann <nico.lehmannm@gmail.com>
2021-01-29 21:23:10 -0800

    Validators for roberto

---
+++ roberto/attic/validators/st3.cpp
@@ -0,0 +1,28 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+  registerValidation();
+
+  long H = inf.readLong(0LL, 1000000LL, "H");
+  inf.readSpace();
+  long W = inf.readLong(0LL, 1000000LL, "W");
+  inf.readSpace();
+  long M = inf.readLong(0LL, H, "M");
+  inf.readSpace();
+  long N = inf.readLong(0LL, W, "N");
+  inf.readEoln();
+  ensure(M * N <= 1000000);
+
+  ensure(H == 1 && M == 1);
+
+  for (int i = 0; i < H; ++i) {
+    for (int j = 0; j < W; ++j) {
+      if (j > 0) inf.readSpace();
+      inf.readInt(0, 1000000);
+    }
+    inf.readEoln();
+  }
+  inf.readEof();
+}
---
+++ roberto/attic/validators/st4.cpp
@@ -0,0 +1,26 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+  registerValidation();
+
+  long H = inf.readLong(0LL, 1000000LL, "H");
+  inf.readSpace();
+  long W = inf.readLong(0LL, 1000000LL, "W");
+  inf.readSpace();
+  long M = inf.readLong(0LL, H, "M");
+  inf.readSpace();
+  long N = inf.readLong(0LL, W, "N");
+  inf.readEoln();
+  ensure(M * N <= 1000000);
+
+  for (int i = 0; i < H; ++i) {
+    for (int j = 0; j < W; ++j) {
+      if (j > 0) inf.readSpace();
+      inf.readInt(0, 1000000);
+    }
+    inf.readEoln();
+  }
+  inf.readEof();
+}
