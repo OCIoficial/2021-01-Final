5654b25cddbb08a79d45c6967b5160b97bad7fb2
Nico Lehmann <nico.lehmannm@gmail.com>
2021-01-30 16:05:27 -0800

    Corrections noticed during contest

--- catalonia/solutions/partial/nlehmann_8pts.cpp
+++ catalonia/solutions/partial/nlehmann_8pts.cpp
@@ -6,10 +6,11 @@ int main() {
   cin >> a >> b;
 
   int ring = 0;
-  int count = 1;
+  long long count = 1;
   while (count <= b) {
     count += 6 * ring;
     ring += 1;
   }
   printf("%d\n", ring - 1);
+  return 0;
 }
